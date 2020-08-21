6c75c46091f721a8d53b4e8c7c2be81d73331e32
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    exynos9810-common: TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- common.mk
+++ common.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_sensorndkbridge
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.samsung
