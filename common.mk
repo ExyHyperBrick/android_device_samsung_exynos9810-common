445b4c3ee6603683f5ca61ab32b802a2e3eaa0ae
Andreas Schneider <asn@cryptomilk.org>
2020-09-03 16:47:20 +0200

    exynos9810-common: thermal: build HAL
    Change-Id: I67630556e3a241ae1391286565d4b9357e64787c

--- common.mk
+++ common.mk
@@ -202,6 +202,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_sensorndkbridge
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.samsung
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.samsung
