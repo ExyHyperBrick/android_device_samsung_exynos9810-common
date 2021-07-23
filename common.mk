9dc80d4b79738824aba2ee09856e07c693ca7177
Tim Zimmermann <tim@linux4.de>
2021-07-23 17:50:27 +0200

    exynos9810-common: drop TetheringOverlay
    * Not needed at all
    
    Change-Id: I80c43411409ca21d15468dc49fe8ec44c04caa18

--- common.mk
+++ common.mk
@@ -264,10 +264,6 @@ PRODUCT_PACKAGES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.samsung
