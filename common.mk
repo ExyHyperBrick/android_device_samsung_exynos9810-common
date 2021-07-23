45032f4cba469578ce0067de12810e4bf9102e12
Tim Zimmermann <tim@linux4.de>
2021-07-23 17:50:27 +0200

    exynos9810-common: drop TetheringOverlay
    * Not needed at all
    
    Change-Id: I80c43411409ca21d15468dc49fe8ec44c04caa18
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -308,10 +308,6 @@ PRODUCT_PACKAGES += \
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Stagefright
 PRODUCT_PACKAGES += \
     libstagefrighthw
