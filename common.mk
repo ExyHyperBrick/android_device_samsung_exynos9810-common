035cac345522a098c0551c2a63cb151bf840323b
Tim Zimmermann <tim@linux4.de>
2022-05-03 20:28:33 +0200

    exynos9810-common: Drop SoundTrigger HAL
    Change-Id: Id50bf7d36ef16332d5b4535bea3569887074b47e
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.0-impl:32 \
     android.hidl.allocator@1.0.vendor:32 \
     audio.bluetooth.default \
     audio.r_submix.default \
