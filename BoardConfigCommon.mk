d7859980e4995420345ed248dd835ab794a0162b
Tim Zimmermann <tim@linux4.de>
2024-07-30 05:57:11 +0200

    exynos9810-common: Switch to hardware/samsung FCM
    Change-Id: Id915542a0b5c4d27e488cae0d97ed36d299a428b
    Signed-off-by: krazey <admin@krazey.de>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := false
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
