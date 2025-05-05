f5599641c8e100b45a8c51f780b7c225dbb4a2b9
Localhorst04 <aaron.laib@protonmail.com>
2025-05-05 22:06:46 +0000

    exynos9810-common: Build android.hardware.radio from source
    this commit replaced android.hardware.radio@1.4.vendor and builds
    vendor.samsung.hardware.radio@2.1 instead to fix following linker error:
    
    F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/rild: library vendor.samsung.hardware.radio@2.1.so not found:
    needed by /vendor/lib64/libril-samsung.so in namespace (default)
    
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -244,7 +244,8 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4.vendor:64 \
+    vendor.samsung.hardware.radio@2.0.vendor:64 \
+    vendor.samsung.hardware.radio@2.1.vendor:64 \
     android.hardware.radio.config@1.2.vendor:64 \
     android.hardware.radio.deprecated@1.0.vendor:64 \
     libnetutils.vendor:64 \
