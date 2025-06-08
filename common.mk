f17e48e54a04cfe3dd46b2fd85c76346289b4c5a
krazey <admin@krazey.de>
2025-06-08 12:48:56 +0200

    exynos9810-common: ril: Build missing libnetutils and libsqlite
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -247,6 +247,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor:64 \
     android.hardware.radio.config@1.2.vendor:64 \
     android.hardware.radio.deprecated@1.0.vendor:64 \
+    libnetutils.vendor:64 \
+    libsqlite.vendor:64 \
     secril_config_svc
 
 # SamsungDoze
