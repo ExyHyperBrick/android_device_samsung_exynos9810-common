84cf58400eef0c45b1507a6e8c6a02b44d84f925
krazey <admin@krazey.de>
2025-06-11 18:45:28 +0200

    Revert "exynos9810-common: Bump android.hardware.keymaster to 4.0 to match with hardware/samsung/"
    not needed
    
    This reverts commit 7ca57bfeba9aa07aae968bc2992b152989335a15.
    
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -134,7 +134,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-service \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl
 
 # Lights
 PRODUCT_PACKAGES += \
