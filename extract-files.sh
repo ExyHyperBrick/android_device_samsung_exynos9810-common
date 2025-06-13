d3b8fbb3c9a6464020dc52e36b0199b200f69466
krazey <admin@krazey.de>
2025-06-13 22:28:58 +0200

    WIP: Use linaro BSP, crypto, sensors, etc
    Signed-off-by: krazey <admin@krazey.de>

--- extract-files.sh
+++ extract-files.sh
@@ -66,11 +66,7 @@ function blob_fixup() {
         vendor/lib*/libsensorlistener.so)
                 "${PATCHELF}" --add-needed libshim_sensorndkbridge.so "${2}"
                 ;;
-        vendor/lib*/hw/hwcomposer.exynos9810.so)
-                "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
-                "${PATCHELF}" --replace-needed libion.so libion-v34.so "${2}"
-                ;;
-        vendor/lib*/libexynosdisplay.so|vendor/lib*/sensors.*.so)
+        vendor/lib*/sensors.*.so)
                 "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
                 ;;
         vendor/lib64/libsec-ril.so)
