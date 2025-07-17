122ad344da2e5b3e16d6343943d8ee50abe16ae3
krazey <admin@krazey.de>
2025-07-17 20:59:13 +0200

    Revert "exynos9810-common: libsec-ril: Always return an empty value for GET_SMSC_ADDRESS"
    This reverts commit 26ca4d5bb62b9f08392b7295db41a032bb45447e.
    
    * Not needed anymore
      Reverting to R RIL blobs
    
    Signed-off-by: krazey <admin@krazey.de>

--- extract-files.py
+++ extract-files.py
@@ -70,10 +70,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/sensors.grip.so',
     ): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
-
-#    'vendor/lib64/libsec-ril.so': blob_fixup()
-#	.sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
-#            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
 }
 
 module = ExtractUtilsModule(
