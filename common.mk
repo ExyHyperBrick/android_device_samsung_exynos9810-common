f127ada14c88827fbc8d0e126db5dbaa22179dc3
Erick04 <solonsosua@gmail.com>
2025-08-11 00:27:22 +0200

    exynos9810-common: Switch to Touch AIDL
    touch: Migrate to AIDL
    Change-Id: Ie0c2dd615652e80032393b04112fc39803205814
    
    On hardware_samsung

--- common.mk
+++ common.mk
@@ -309,7 +309,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 # Update
 AB_OTA_UPDATER := false
