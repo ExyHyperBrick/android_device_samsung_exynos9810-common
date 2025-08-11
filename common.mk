77ab5d947397a35445167db0b9f660a1bc59d830
Erick04 <solonsosua@gmail.com>
2025-08-12 00:28:34 +0200

    exynos9810-common: Use 32bit camera hal
    Use 32bit camera hal
    This fixes selfie camera
    
    Change-Id: I8c32aa6df7122049d2d2c2a8fb1577d61d1967ee
    Co-Authored-By: opensourcefreak <109588676+opensourcefreak@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.samsung \
+    android.hardware.camera.provider@2.5-service.samsung \
     libGrallocMapperCamera
 
 # Charger
