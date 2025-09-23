d69ec38013d3619578a080c923cf83892b9a088f
Erick04 <solonsosua@gmail.com>
2025-09-23 12:24:49 +0200

    exynos9810-common: Drop TARGET_KEYMASTER_VARIANT flag and set soong params
    Samsung keymaster HAL moved to blueprint.
    
    Change-Id: I992a12f1ddc5769ddb9fb232ea2a287c1d654da6

--- common.mk
+++ common.mk
@@ -142,6 +142,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@3.0-impl
+    
+$(call soong_config_set,samsungVars,target_keymaster3_library,//vendor/samsung/exynos9810-common:libskeymaster3device)
 
 # Lights
 PRODUCT_PACKAGES += \
