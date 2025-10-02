b4bff186fd6349e6b4a728d6de1fce75d11b1c2a
Mathias Gluszczynski <admin@krazey.de>
2025-10-02 14:42:15 +0200

    exynos9810-common: Restart audioserver once after boot
    * workaround for the mixer control race
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/fstab.samsungexynos9810:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos9810 \
     $(COMMON_PATH)/configs/init/fstab.samsungexynos9810:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.samsungexynos9810 \
+    $(COMMON_PATH)/configs/init/audio_postboot.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/audio_postboot.rc \
     $(COMMON_PATH)/configs/init/mobicore.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/mobicore.rc \
     $(COMMON_PATH)/configs/init/init.baseband.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.baseband.rc \
     $(COMMON_PATH)/configs/init/init.greenfix.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.greenfix.rc \
