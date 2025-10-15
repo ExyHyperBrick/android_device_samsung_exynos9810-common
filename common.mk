0b77826f47def807d8b9c012e0a1d50ce1a8cd1e
Mathias Gluszczynski <admin@krazey.de>
2025-10-15 19:56:09 +0200

    exynos9810-common: init: Tune VM/LMK for zram-backed swap
    - Raise allocator reserve: vm.min_free_kbytes=64 MiB - Disable swap
      readahead on zram: vm.page_cluster=0 - Prefer swapping cold pages:
      vm.swappiness=100 - Earlier LMK trims (MiB): 96,120,144,168,256,384 ->
      /sys/module/lowmemorykiller/parameters/minfree=24576,30720,36864,43008,65536,98304
    
    Rationale: keep headroom under pressure (avoids binder starvation),
    reduce swap thrash on zram, and bias toward swapping idle pages rather
    than killing foreground apps.
    
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -126,6 +126,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.rc \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.usb.rc \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsung.rc \
+    $(COMMON_PATH)/configs/init/init.swap.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.swap.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(COMMON_PATH)/configs/init/wifi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/wifi.rc
 
