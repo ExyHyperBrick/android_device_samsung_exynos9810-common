0db7307683ddc034fe86b6649f4954c8f2e13f78
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    exynos9810-common: Declare Samsung camera quirks as bool vars
    Adapted-from: LineageOS/android_device_samsung_exynos9820-common@b6ff2d002b7d34a4c648979cea49f1099f747b55

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,8 +50,8 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET)
 
 # Camera
-$(call soong_config_set,samsungCameraVars,usage_64bit,true)
-$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
+$(call soong_config_set_bool,samsungCameraVars,usage_64bit,true)
+$(call soong_config_set_bool,samsungCameraVars,needs_sec_reserved_field,true)
 
 # Display
 BOARD_MINIMUM_DISPLAY_BRIGHTNESS := 1
