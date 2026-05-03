5a771910fc6e10fc64e5d2365f9af5bea96b01ce
krazey <admin@krazey.de>
2026-05-03 19:42:21 +0200

    Revert "exynos9810-common: init: Enable schedtune v1 groups with sane defaults"
    This reverts commit e61bb0cf8b260c2fb60821fcb2138211c1bec2c8.
    
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -127,7 +127,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.rc \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.usb.rc \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsung.rc \
-    $(COMMON_PATH)/configs/init/init.stune.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.stune.rc \
     $(COMMON_PATH)/configs/init/init.swap.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.swap.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(COMMON_PATH)/configs/init/wifi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/wifi.rc
