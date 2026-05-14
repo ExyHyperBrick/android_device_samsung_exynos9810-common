928959036b62885377abcf588aa5c2ffbe569d23
krazey <admin@krazey.de>
2026-05-14 14:00:00 +0200

    exynos9810-common: Ship scheduler service from VNDK prebuilts
    Signed-off-by: krazey <admin@krazey.de>

--- common.mk
+++ common.mk
@@ -308,9 +308,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.schedulerservice@1.0.vendor:64 \
     android.hardware.sensors@1.0-impl.samsung:64 \
     android.hardware.sensors@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/vndk/android.frameworks.schedulerservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.schedulerservice@1.0.so
 
 # Shims
 PRODUCT_PACKAGES += \
