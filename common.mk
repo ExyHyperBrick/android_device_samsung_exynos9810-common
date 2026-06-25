5f654cb1aa5ab1df1cc508c1a065990c76448cca
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:19:11 -0400

    exynos9810-common: Stop disabling configstore
    * As of CP2A it's just dead and gone.
    
    Change-Id: I671b668c4bb9a967f779753233c2025a428c9421

--- common.mk
+++ common.mk
@@ -68,10 +68,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images_vendor
 
-# ConfigStore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
